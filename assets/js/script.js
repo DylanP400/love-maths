7772015fd02dd41508f6f236e79eab09dd14d411
Dylan <Dylanmcmahon187@gmail.com>
2023-01-13 22:16:42 +0000

    Add event listeners for document 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and ass ecent liseners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
+
+
+
 
 /**
  * The main game "loop", called when the script is first loaded
